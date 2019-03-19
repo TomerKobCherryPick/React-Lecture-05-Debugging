b844fc0a4b17646f5ff3dceb78eca44cdd73724f
Tomer <tomerkobcherrypick@gmail.com>
2019-03-19 17:20:55 +0200

    changed minor implentation for solving bug 1 problem

--- bugs/1.js
+++ bugs/1.js
@@ -14,14 +14,18 @@ const styles = StyleSheet.create({
   }
 });
 
-const Container = props => <Greeting style={styles.center} />;
+const Container = props => (
+  <View style={styles.container}>
+    <Greeting style={styles.center} />
+  </View>
+)
 
 /////
 // Do not edit anything above this line
 /////
 
 const Greeting = props => (
-  <Text style={styles.greeting}>
+  <Text style={[styles.greeting, props.style]}>
     This text should be red and centered vertically
   </Text>
 );
